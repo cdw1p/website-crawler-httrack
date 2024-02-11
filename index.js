93f9b2caa8053afaee9441955265c837f0cbc88e
Cahyo Dwi Putro <cahyorizqullah@gmail.com>
2024-02-11 13:00:43 +0700

    chore(fix): data result

--- index.js
+++ index.js
@@ -13,12 +13,25 @@ const startUrl = '' // Your target website
 const visitedUrls = new Set()
 
 /**
+ * @description: empty file function
+ */
+const initFile = () => new Promise(async (resolve, reject) => {
+  const filePath = path.join(__dirname, 'data.txt')
+  try {
+    await fs.unlinkSync(filePath)
+    resolve(true)
+  } catch (err) { }
+  await fs.writeFileSync(filePath, '')
+  resolve(true)
+})
+
+/**
  * @param {*} data 
  * @description: save result to file function
  */
 const saveResultToFile = (data) => new Promise(async (resolve, reject) => {
   const filePath = path.join(__dirname, 'data.txt')
-  await fs.writeFileSync(filePath, data, 'utf8')
+  await fs.appendFileSync(filePath, `${data}\n`)
   resolve(true)
 })
 
@@ -38,15 +51,16 @@ const crawlWebsite = (url, depth = 0) => new Promise(async (resolve, reject) => 
     for (let link of links) {
       const absoluteLink = (new URL(link, url).href).split('#')[0]
       const shouldLink = (new URL(absoluteLink)).origin === (new URL(url)).origin
-      if (shouldLink && !(visitedUrls.has(absoluteLink))) {
+      const notImages = !(absoluteLink.match(/\.(jpeg|jpg|gif|png|svg)$/))
+      if (shouldLink && notImages && !(visitedUrls.has(absoluteLink))) {
         crawlWebsite(absoluteLink, depth + 1)
       }
     }
+    await saveResultToFile(url)
     console.log(`[${moment().format('HH:mm:ss')}] Visited: ${url}`)
   } catch (error) {
     console.error(`[${moment().format('HH:mm:ss')}] Failed to fetch ${url}: ${error.message}`.red)
   }
-  resolve(true)
 })
 
 /**
@@ -55,8 +69,8 @@ const crawlWebsite = (url, depth = 0) => new Promise(async (resolve, reject) => 
  */
 const run = async (url) => {
   try {
+    await initFile()
     await crawlWebsite(url)
-    await saveResultToFile(Array.from(visitedUrls).join('\n'))
   } catch (error) {
     console.error(`[${moment().format('HH:mm:ss')}] Failed to fetch ${url}: ${error.message}`.red)
   }
